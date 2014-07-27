8ea6176719cd0c167420e3a7332efc7ece947a0d
lovedaybrooke <katmatfield@kats-mbp.home>
2014-07-27 22:44:36 +0100

    Handle form submits without ad text

--- genderjobcheck/views.py
+++ genderjobcheck/views.py
@@ -1,5 +1,6 @@
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 import assess
 
@@ -11,6 +12,9 @@ def home(request):
 def assessJobAd(request):
     if request.method == 'POST':
         ad_text = request.POST["adtext"]
-        results = assess.assess(ad_text)        
-        return render(request, 'results.html', results
-        	)
+        if len(ad_text):
+            results = assess.assess(ad_text)        
+            return render(request, 'results.html', results
+                )
+        else:
+           	return redirect('/')
