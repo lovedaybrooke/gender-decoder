e0a4a2086ffbd4b0923a7395cb7cab7e0e1d8216
lovedaybrooke <katmatfield@gmail.com>
2016-01-23 15:56:35 +0000

    Update urls and views to take account of reference letters

--- genderjobcheck/urls.py
+++ genderjobcheck/urls.py
@@ -6,7 +6,15 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.home),
     url(r'^about$', views.about),
-    url(r'^assess$', csrf_exempt(views.assessJobAd)),
-    url(r"^results/(?P<ad_id>[0-9a-z]+)$", csrf_exempt(views.results),
-        name="results"),
+    url(r'^ref-letters$', views.ref_letters),
+    url(r'^job-adverts$', views.job_adverts),
+    url(r'^job-adverts/assess$', csrf_exempt(views.assessJobAd)),
+    url(r"^job-advert/results/(?P<ad_id>[0-9a-z]+)$", csrf_exempt(
+        views.job_advert_results), name="job_advert_results"),
+    url(r'^job-adverts/word-lists$', views.job_adverts_word_lists),
+    url(r'^ref-letters$', views.ref_letters),
+    url(r'^ref-letters/assess$', csrf_exempt(views.assess_ref_letter)),
+    url(r"^ref-letter/results/(?P<letter_id>[0-9a-z]+)$", csrf_exempt(
+        views.ref_letter_results), name="ref-letter-results"),
+    url(r'^ref-letters/word-lists$', views.ref_letters_word_lists),
 )
--- genderjobcheck/views.py
+++ genderjobcheck/views.py
@@ -9,6 +9,13 @@ def home(request):
     if request.method == 'GET':
         return render(request, 'home.html', {})
 
+def job_adverts(request):
+    if request.method == 'GET':
+        return render(request, 'job-adverts.html', {})
+
+def ref_letters(request):
+    if request.method == 'GET':
+        return render(request, 'ref-letters.html', {})
 
 @csrf_exempt
 def assessJobAd(request):
@@ -16,19 +23,49 @@ def assessJobAd(request):
         ad_text = request.POST["texttotest"]
         if len(ad_text):
             job_ad = JobAd.create(ad_text)
-            return redirect("results", ad_id=job_ad.hash)
+            return redirect("job_advert_results", ad_id=job_ad.hash)
         else:
-            return redirect('/')
+            return redirect('/job-adverts')
 
 
-def results(request, ad_id):
+def job_advert_results(request, ad_id):
     job_ad = get_object_or_404(JobAd, hash=ad_id)
-    return render(request, 'results.html', job_ad.results_dictionary())
+    print(job_ad)
+    return render(request, 'job-advert-results.html', job_ad.results_dictionary())
 
-
-def about(request):
+def job_adverts_word_lists(request):
     if request.method == 'GET':
-        return render(request, 'about.html',
+        return render(request, 'job-adverts-word-lists.html',
             {"masculine_coded_words": wordlists.masculine_coded_words,
             "feminine_coded_words": wordlists.feminine_coded_words}
         )
+
+@csrf_exempt
+def assess_ref_letter(request):
+    if request.method == 'POST':
+        gender = request.POST["gender"]
+        if not gender:
+            gender = "unknown"
+        letter_text = request.POST["texttotest"]
+        if len(letter_text):
+            ref_letter = RefLetter.create(letter_text, gender)
+            return redirect("ref-letter-results", letter_id=ref_letter.hash)
+        else:
+            return redirect('/ref-letters')
+
+
+def ref_letter_results(request, letter_id):
+    ref_letter = get_object_or_404(RefLetter, hash=letter_id)
+    return render(request, 'ref-letter-results.html', ref_letter.results_dictionary())
+
+def ref_letters_word_lists(request):
+    if request.method == 'GET':
+        return render(request, 'ref-letters-word-lists.html',
+            {"outstanding_words": wordlists.outstanding_words,
+            "ability_words": wordlists.ability_words,
+            "grindstone_words": wordlists.grindstone_words}
+        )
+
+def about(request):
+    if request.method == 'GET':
+        return render(request, 'about.html', {})
