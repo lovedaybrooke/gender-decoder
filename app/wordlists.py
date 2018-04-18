0075da0614bf64df094a4b5cf0dfe8854767d270
Kat Matfield <katmatfield@gmail.com>
2018-04-18 16:28:50 +0100

    Clarify explanation of coded words

--- app/wordlists.py
+++ app/wordlists.py
@@ -128,31 +128,33 @@ possible_codings = (
 
 explanations = {
     "feminine-coded": ("This job ad uses more words that "
-                        "are stereotypically feminine than words that are "
-                        "stereotypically masculine. Fortunately, the research "
-                        "suggests this will have only a slight effect on how "
-                        "appealing the job is to men, and will encourage women"
-                        " applicants."),
+                        "are subtly coded as feminine than words that are "
+                        "subtly coded as masculine (according to the "
+                        "research). Fortunately, the research suggests "
+                        "this will have only a slight effect on how appealing "
+                        "the job is to men, and will encourage women applicants."),
     "masculine-coded": ("This job ad uses more words that "
-                        "are stereotypically masculine than words that are "
-                        "stereotypically feminine. It risks putting women off "
-                        "applying, but will probably encourage men to apply."),
+                        "are subtly coded as masculine than words that are "
+                        "subtly coded as feminine (according to the research). "
+                        "It risks putting women off applying, but will probably "
+                        "encourage men to apply."),
     "strongly feminine-coded": ("This job ad uses more words that "
-                        "are stereotypically feminine than words that are "
-                        "stereotypically masculine. Fortunately, the research "
-                        "suggests this will have only a slight effect on how "
-                        "appealing the job is to men, and will encourage women"
-                        " applicants."),
+                        "are subtly coded as feminine than words that are "
+                        "subtly coded as masculine (according to the "
+                        "research). Fortunately, the research suggests this "
+                        "will have only a slight effect on how appealing the "
+                        "job is to men, and will encourage women applicants."),
     "strongly masculine-coded": ("This job ad uses more words that "
-                        "are stereotypically masculine than words that are "
-                        "stereotypically feminine. It risks putting women off "
-                        "applying, but will probably encourage men to apply."),
+                        "are subtly coded as masculine than words that are subtly "
+                        "coded as feminine (according to the research). It risks "
+                        "putting women off applying, but will probably encourage "
+                        "men to apply."),
     "empty": ("This job ad doesn't use any words "
-              "that are stereotypically masculine and stereotypically "
-              "feminine. It probably won't be off-putting to men or women "
-              "applicants."),
+              "that are subtly coded as masculine or feminine (according to "
+              "the research). It probably won't be off-putting to men or "
+              "women applicants."),
     "neutral": ("This job ad uses an equal number "
-              "of words that are stereotypically masculine and "
-              "stereotypically feminine. It probably won't be off-putting "
+              "of words that are subtly coded as masculine and feminine "
+              "(according to the research). It probably won't be off-putting "
               "to men or women applicants.")
 }
